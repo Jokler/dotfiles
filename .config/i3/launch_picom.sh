767fbcb719e941d3b9a09b68c0a1c63dd2a57d30
Jokler <jokler@protonmail.com>
2020-01-10 02:43:27 +0100

    Switch from compton to picom

--- .config/i3/launch_picom.sh
+++ .config/i3/launch_picom.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Terminate already running bar instances
-killall -q compton
+killall -q picom
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x compton >/dev/null; do sleep 1; done
+while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-compton &
+picom --invert-color-include '(class_g="weka-gui-GUIChooser")' &
 
-echo "compton launched..."
+echo "picom launched..."
